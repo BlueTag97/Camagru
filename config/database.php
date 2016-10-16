9f0c75d5b78074e90cd00bfb66b67e7891d4a753
Emmanuel MHLABA <emmmhla@e5r8p4.jnb.42.fr>
2016-10-16 13:49:18 +0200

    Added PDO DSN info

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+
+$DB_USER = "root";
+$DB_PASSWORD = "TartMuffin97";
+
+$DB_SERVER = "127.0.0.1";
+$DB_PORT = "3306";
+$DB_CHARSET = "utf8";
+$DB_DSN = "mysql:host=$DB_SERVER;charset=$DB_CHARSET;port=$DB_PORT";
+
+?>
